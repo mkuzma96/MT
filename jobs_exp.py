119b33bfa6d434eff23e485137895bae08fd8514
Milan Kuzmanovic <33872866+mkuzma96@users.noreply.github.com>
2021-10-08 14:19:18 +0200

    Update jobs_exp.py

--- jobs_exp.py
+++ jobs_exp.py
@@ -7,13 +7,10 @@ The code contains experiments with Jobs dataset
 
 #%% Packages
 
-import os
 import numpy as np
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
-
-os.chdir('C:/Users/Milan/Dropbox/MK/ETH PhD Applied Probabilistic AI/projects_long/MT/code_new/')
 
 from main import *
 from benchmarks import *
